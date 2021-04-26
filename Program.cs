2be0116f6b1246093daacb4bf1f744cc23a276c0
numbasan-san <eddymanuel20@gmail.com>
2021-04-25 23:16:30 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Media;
+
+namespace TetrisCSharp
+{
+    class Program
+    {
+
+        public static int[,] area = new int[23, 10];
+        public static int[,] posicionCaidaPieza = new int[32, 10];
+        public static string fragmentoFigura = "#";
+        public static Stopwatch temporizador = new Stopwatch();
+        public static Stopwatch tempoFall = new Stopwatch();
+        public static Stopwatch tempoIn = new Stopwatch();
+        public static int tiempoFall, veloFall = 300;
+        public static bool letFall = false;
+        static tetraPieza pieza;
+        static tetraPieza piezaSig;
+        public static ConsoleKeyInfo tecla;
+        public static bool teclaPulsada = false;
+        public static int lineas = 0, puntos = 0, nivel = 1;
+
+        static void Main()
+        {
+
+            Pantalla();
+
+            Console.SetCursorPosition(4, 5);
+            Console.WriteLine("Pulse una tecla");
+            Console.ReadKey(true);
+            temporizador.Start();
+            tempoFall.Start();
+            long time = temporizador.ElapsedMilliseconds;
+            Console.SetCursorPosition(25, 0);
+            Console.WriteLine("Nivel: " + nivel);
+            Console.SetCursorPosition(25, 1);
+            Console.WriteLine("Puntos: " + puntos);
+            Console.SetCursorPosition(25, 2);
+            Console.WriteLine("Líneas: " + lineas);
+            piezaSig = new tetraPieza();
+            pieza = piezaSig;
+            pieza.Aparecer();
+            piezaSig = new tetraPieza();
+
+            Actualizar();
+
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Game Over \n Otra partida? (Y/N)");
+            string teclaPulsada = Console.ReadLine();
+
+            if (teclaPulsada.ToUpper() == "Y")
+            {
+                int[,] areaJuego = new int[23, 10];
+                posicionCaidaPieza  = new int[23, 10];
+                temporizador = new Stopwatch();
+                tempoFall = new Stopwatch();
+                tempoIn = new Stopwatch();
+                veloFall = 300;
+                letFall = false;
+                Program.teclaPulsada = false;
+                lineas = 0;
+                puntos = 0;
+                nivel = 1;
+                GC.Collect();
+                Console.Clear();
+                Main();
+            }
+            else return;
+
+        }
+
+        public static void Pantalla()
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            for (int length = 0; length <= 22; ++ length)
+            {
+
+                Console.SetCursorPosition(0, length);
+                Console.Write("|");
+                Console.SetCursorPosition(21, length);
+                Console.Write("|");
+
+            }
+
+            Console.SetCursorPosition(0, 23);
+
+            for (int width = 0; width <= 10; ++ width)
+            {
+
+                Console.Write("__");
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+                
+        public static void Actualizar()
+        {
+
+            while (true)
+            {
+
+                tiempoFall = (int)tempoFall.ElapsedMilliseconds;
+                
+                if (tiempoFall > veloFall)
+                {
+
+                    tiempoFall = 0;
+                    tempoFall.Restart();
+                    pieza.Soltar();
+
+                }
+
+                if (letFall == true)
+                {
+
+                    pieza = piezaSig;
+                    piezaSig = new tetraPieza();
+                    pieza.Aparecer();
+
+                    letFall = false;
+
+                }
+
+                int i;
+                for (i = 0; i < 10; i++)
+                {
+
+                    if (posicionCaidaPieza[0, i] == 1)
+                        
+                        return;
+
+                }
+
+                Pulso();
+                Mover();
+
+            }
+
+        }
+
+        private static void Pulso()
+        {
+            if (Console.KeyAvailable)
+            {
+                tecla = Console.ReadKey();
+                teclaPulsada = true;
+            }
+            else
+                teclaPulsada = false;
+
+            if (Program.tecla.Key == ConsoleKey.LeftArrow & !pieza.HayAlgoIzquierda() & teclaPulsada)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    pieza.posicion[i][1] -= 1;
+                }
+                pieza.Actualizar();
+
+            }
+            else if (Program.tecla.Key == ConsoleKey.RightArrow & !pieza.HayAlgoDerecha() & teclaPulsada)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    pieza.posicion[i][1] += 1;
+                }
+                pieza.Actualizar();
+            }
+            if (Program.tecla.Key == ConsoleKey.DownArrow & teclaPulsada)
+            {
+                pieza.Soltar();
+            }
+            if (Program.tecla.Key == ConsoleKey.DownArrow & teclaPulsada)
+            {
+                for (; pieza.HayAlgoDebajo() != true;)
+                {
+                    pieza.Soltar();
+                }
+            }
+            if (Program.tecla.Key == ConsoleKey.UpArrow & teclaPulsada)
+            {
+
+                pieza.Rotar();
+                pieza.Actualizar();
+            }
+        }
+
+        private static void Mover()
+        {
+
+            int combo = 0;
+
+            for (int i = 0; i < 23; i++)
+            {
+
+                int j;
+
+                for (j = 0; j < 10; j++)
+                {
+
+                    if (posicionCaidaPieza[i, j] == 0)
+                        break;
+
+                }
+
+                if (j == 10)
+                {
+
+                    lineas++;
+                    combo++;
+
+                    for (j = 0; j < 10; j++)
+                    {
+
+                        posicionCaidaPieza[i, j] = 0;
+
+                    }
+
+                    int[,] newdroppedtetrominoeLocationGrid = new int[23, 10];
+
+                    for (int k = 1; k < i; k++)
+                    {
+
+                        for (int l = 0; l < 10; l++)
+                        {
+
+                            newdroppedtetrominoeLocationGrid[k + 1, l] = posicionCaidaPieza[k, l];
+
+                        }
+
+                    }
+
+                    for (int k = 1; k < i; k++)
+                    {
+
+                        for (int l = 0; l < 10; l++)
+                        {
+
+                            posicionCaidaPieza[k, l] = 0;
+
+                        }
+
+                    }
+
+                    for (int k = 0; k < 23; k++)
+                        for (int l = 0; l < 10; l++)
+                            if (newdroppedtetrominoeLocationGrid[k, l] == 1)
+                                posicionCaidaPieza[k, l] = 1;
+                    Dibujar();
+
+                }
+
+            }
+            if (combo == 1)
+                puntos += 40 * nivel;
+            else if (combo == 2)
+                puntos += 100 * nivel;
+            else if (combo == 3)
+                puntos += 300 * nivel;
+            else if (combo > 3)
+                puntos += 300 * combo * nivel;
+
+            if (lineas < 5) nivel = 1;
+            else if (lineas < 10) nivel = 2;
+            else if (lineas < 15) nivel = 3;
+            else if (lineas < 25) nivel = 4;
+            else if (lineas < 35) nivel = 5;
+            else if (lineas < 50) nivel = 6;
+            else if (lineas < 70) nivel = 7;
+            else if (lineas < 90) nivel = 8;
+            else if (lineas < 110) nivel = 9;
+            else if (lineas < 150) nivel = 10;
+
+
+            if (combo > 0)
+            {
+
+                Console.SetCursorPosition(25, 0);
+                Console.WriteLine("Nivel: " + nivel);
+                Console.SetCursorPosition(25, 1);
+                Console.WriteLine("Puntos: " + puntos);
+                Console.SetCursorPosition(25, 2);
+                Console.WriteLine("Lineas: " + lineas);
+
+            }
+
+            veloFall = 300 - 22 * nivel;
+
+        }
+
+        public static void Dibujar()
+        {
+
+            for (int i = 0; i < 23; ++i)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+
+                    Console.SetCursorPosition(1 + 2 * j, i);
+                    if (area[i, j] == 1 | posicionCaidaPieza[i, j] == 1)
+                    {
+
+                        Console.SetCursorPosition(1 + 2 * j, i);
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(fragmentoFigura);
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    }
+                    else
+                    {
+
+                        Console.Write("  ");
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ tetraPieza.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace TetrisCSharp
+{
+    class tetraPieza
+    {
+        public static int[,] I = new int[1, 4] { { 1, 1, 1, 1 } };
+        public static int[,] O = new int[2, 2] { { 1, 1 }, { 1, 1 } };
+        public static int[,] T = new int[2, 3] { { 0, 1, 0 }, { 1, 1, 1 } };
+        public static int[,] S = new int[2, 3] { { 0, 1, 1 }, { 1, 1, 0 } };
+        public static int[,] Z = new int[2, 3] { { 1, 1, 0 }, { 0, 1, 1 } };
+        public static int[,] J = new int[2, 3] { { 1, 0, 0 }, { 1, 1, 1 } };
+        public static int[,] L = new int[2, 3] { { 0, 0, 1 }, { 1, 1, 1 } };
+        public static List<int[,]> tetrimino = new List<int[,]>() { I, O, T, S, Z, J, L };
+
+        private bool isRecto = false;
+        private int[,] figura;
+        public List<int[]> posicion = new List<int[]>();
+
+        public tetraPieza()
+        {
+            //Obtenemos la siguinte pieza y ponemos en su posicion
+            Random random = new Random();
+            figura = tetrimino[random.Next(0, 7)];
+            for (int i = 23; i < 33; ++i)
+            {
+                for (int j = 3; j < 10; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write("  ");
+                }
+
+            }
+            //Repintamos el area de juego
+            Program.Pantalla();
+
+            for (int i = 0; i < figura.GetLength(0); i++)
+            {
+                for (int j = 0; j < figura.GetLength(1); j++)
+                {
+                    if (figura[i, j] == 1)
+                    {
+                        Console.SetCursorPosition(((10 - figura.GetLength(1)) / 2 + j) * 2 + 20, i + 5);
+                        Console.Write(Program.fragmentoFigura);
+                    }
+                }
+            }
+        }
+
+        public void Aparecer()
+        {
+            for (int i = 0; i < figura.GetLength(0); i++)
+            {
+                for (int j = 0; j < figura.GetLength(1); j++)
+                {
+                    if (figura[i, j] == 1)
+                    {
+                        posicion.Add(new int[] { i, (10 - figura.GetLength(1)) / 2 + j });
+                    }
+                }
+            }
+            Actualizar();
+        }
+
+        public void Soltar()
+        {
+
+            if (HayAlgoDebajo())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Program.posicionCaidaPieza[posicion[i][0], posicion[i][1]] = 1;
+                }
+                Program.letFall= true;
+
+            }
+            else
+            {
+                for (int numCount = 0; numCount < 4; numCount++)
+                {
+                    posicion[numCount][0] += 1;
+                }
+                Actualizar();
+            }
+        }
+
+        public void Rotar()
+        {
+            List<int[]> posicionTemporal = new List<int[]>();
+            for (int i = 0; i < figura.GetLength(0); i++)
+            {
+                for (int j = 0; j < figura.GetLength(1); j++)
+                {
+                    if (figura[i, j] == 1)
+                        posicionTemporal.Add(new int[] { i, (10 - figura.GetLength(1)) / 2 + j });
+
+                }
+            }
+
+            if (figura == tetrimino[0])
+            {
+                if (isRecto == false)
+                {
+                    for (int i = 0; i < posicion.Count; i++)
+                    {
+                        posicionTemporal[i] = TransformMatrix(posicion[i], posicion[2], "AgujasReloj");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < posicion.Count; i++)
+                    {
+                        posicionTemporal[i] = TransformMatrix(posicion[i], posicion[2], "Antihorario");
+                    }
+                }
+            }
+
+            else if (figura == tetrimino[3])
+            {
+                for (int i = 0; i < posicion.Count; i++)
+                {
+                    posicionTemporal[i] = TransformMatrix(posicion[i], posicion[3], "AgujasReloj");
+                }
+            }
+
+            else if (figura == tetrimino[1]) return;
+            else
+            {
+                for (int i = 0; i < posicion.Count; i++)
+                {
+                    posicionTemporal[i] = TransformMatrix(posicion[i], posicion[2], "AgujasReloj");
+                }
+            }
+
+
+            for (int count = 0; SuperponerIzquierda(posicionTemporal) != false | SuperPonerDerecha(posicionTemporal) != false | suporponer(posicionTemporal) != false; count++)
+            {
+                if (SuperponerIzquierda(posicionTemporal) == true)
+                {
+                    for (int i = 0; i < posicion.Count; i++)
+                    {
+                        posicionTemporal[i][1] += 1;
+                    }
+                }
+
+                if (SuperPonerDerecha(posicionTemporal) == true)
+                {
+                    for (int i = 0; i < posicion.Count; i++)
+                    {
+                        posicionTemporal[i][1] -= 1;
+                    }
+                }
+                if (suporponer(posicionTemporal) == true)
+                {
+                    for (int i = 0; i < posicion.Count; i++)
+                    {
+                        posicionTemporal[i][0] -= 1;
+                    }
+                }
+                if (count == 3)
+                {
+                    return;
+                }
+            }
+
+            posicion = posicionTemporal;
+
+        }
+
+        public int[] TransformMatrix(int[] coord, int[] axis, string dir)
+        {
+            int[] pcoord = { coord[0] - axis[0], coord[1] - axis[1] };
+            if (dir == "Antihorario")
+            {
+                pcoord = new int[] { -pcoord[1], pcoord[0] };
+            }
+            else if (dir == "AgujasReloj")
+            {
+                pcoord = new int[] { pcoord[1], -pcoord[0] };
+            }
+
+            return new int[] { pcoord[0] + axis[0], pcoord[1] + axis[1] };
+        }
+
+        public bool HayAlgoDebajo()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (posicion[i][0] + 1 >= 23)
+                    return true;
+                if (posicion[i][0] + 1 < 23)
+                {
+                    if (Program.posicionCaidaPieza[posicion[i][0] + 1, posicion[i][1]] == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool? suporponer(List<int[]> location)
+        {
+            List<int> coordenadas = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                coordenadas.Add(location[i][0]);
+                if (location[i][0] >= 23)
+                    return true;
+                if (location[i][0] < 0)
+                    return null;
+                if (location[i][1] < 0)
+                    return null;
+                if (location[i][1] > 9)
+                    return null;
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (coordenadas.Max() - coordenadas.Min() == 3)
+                {
+                    if (coordenadas.Max() == location[i][0] | coordenadas.Max() - 1 == location[i][0])
+                    {
+                        if (Program.posicionCaidaPieza[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (coordenadas.Max() == location[i][0])
+                    {
+                        if (Program.posicionCaidaPieza[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool HayAlgoIzquierda()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (posicion[i][1] == 0)
+                {
+                    return true;
+                }
+                else if (Program.posicionCaidaPieza[posicion[i][0], posicion[i][1] - 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool? SuperponerIzquierda(List<int[]> location)
+        {
+            List<int> coordenadas = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                coordenadas.Add(location[i][1]);
+                if (location[i][1] < 0)
+                {
+                    return true;
+                }
+                if (location[i][1] > 9)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (coordenadas.Max() - coordenadas.Min() == 3)
+                {
+                    if (coordenadas.Min() == location[i][1] | coordenadas.Min() + 1 == location[i][1])
+                    {
+                        if (Program.posicionCaidaPieza[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (coordenadas.Min() == location[i][1])
+                    {
+                        if (Program.posicionCaidaPieza[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool HayAlgoDerecha()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (posicion[i][1] == 9)
+                {
+                    return true;
+                }
+                else if (Program.posicionCaidaPieza[posicion[i][0], posicion[i][1] + 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool? SuperPonerDerecha(List<int[]> location)
+        {
+            List<int> coordenadas = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                coordenadas.Add(location[i][1]);
+                if (location[i][1] > 9)
+                {
+                    return true;
+                }
+                if (location[i][1] < 0)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (coordenadas.Max() - coordenadas.Min() == 3)
+                {
+                    if (coordenadas.Max() == location[i][1] | coordenadas.Max() - 1 == location[i][1])
+                    {
+                        if (Program.posicionCaidaPieza[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (coordenadas.Max() == location[i][1])
+                    {
+                        if (Program.posicionCaidaPieza[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Actualizar()
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Program.area[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Program.area[posicion[i][0], posicion[i][1]] = 1;
+            }
+            Program.Dibujar();
+        }
+
+    }
+}
